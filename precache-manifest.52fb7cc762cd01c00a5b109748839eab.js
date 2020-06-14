bd27d55242ebe042d2d3e78f8eb303f396de1987
Rohandeshwal <rohandeshwal19@gmail.com>
2020-06-14 21:28:08 +0530

    Updates

---
+++ precache-manifest.52fb7cc762cd01c00a5b109748839eab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dd3ebdc4f69d2bacb3ef79afda8476",
+    "url": "/Learn-react/index.html"
+  },
+  {
+    "revision": "64a9c1d6c378ba947370",
+    "url": "/Learn-react/static/css/main.33dd6a74.chunk.css"
+  },
+  {
+    "revision": "f6be9cd0613123ec86d1",
+    "url": "/Learn-react/static/js/2.0b68901b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Learn-react/static/js/2.0b68901b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a9c1d6c378ba947370",
+    "url": "/Learn-react/static/js/main.4d948de7.chunk.js"
+  },
+  {
+    "revision": "2eeb6b7385fefa6d701b",
+    "url": "/Learn-react/static/js/runtime-main.6bdf8348.js"
+  }
+]);
